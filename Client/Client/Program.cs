4b86f000bab44998e0c09656e7a34bc97b3c8246
Niclas <hjulmandn@gmail.com>
2020-05-25 13:10:12 +0200

    Server/client add

---
+++ Client/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            Console.WriteLine("Port:");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ip:");
+            string ipaddress = Console.ReadLine();
+            IPAddress ip = IPAddress.Parse(ipaddress);
+            IPEndPoint endpoint = new IPEndPoint(ip, port);
+
+            client.Connect(endpoint);
+
+            while (true)
+            {
+
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine("Skriv din besked:");
+                string text = Console.ReadLine();
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+                stream.Write(bytes, 0, bytes.Length);
+            }
+                        
+            client.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 420;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint endpoint = new IPEndPoint(ip, port);
+            TcpListener listener = new TcpListener(endpoint);
+
+            listener.Start();
+
+            Console.WriteLine("Awaiting Clients");
+
+            TcpClient client = listener.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[256];
+
+            while (true)
+            {                                
+                int numb = stream.Read(buffer, 0, buffer.Length);
+
+                string mes = Encoding.UTF8.GetString(buffer, 0, numb);
+                Console.WriteLine(mes);
+            }
+        }
+    }
+}
