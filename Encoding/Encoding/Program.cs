bd6e2f754a3e7759d25dcb00df0b1455aa10c85d
Niclas <hjulmandn@gmail.com>
2020-05-25 09:06:07 +0200

    Initial project

---
+++ Encoding/Encoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bitch!");
+        }
+
+        static void Choice(string choice)
+        {
+
+        }
+    }
+}
